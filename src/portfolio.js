32b29a8612a158893c0de577d8d4ec10c6356ca9
sidharth sahu <siddharthsahu1211@gmail.com>
2026-01-07 05:00:23 +0000

    Update name to Sidharth Sahu

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,8 +20,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "praveen chandar panda",
-  title: "Hi i praveen nayak  , I'm nayak belonges to nayak ",
+  username: "Sidharth Sahu",
+  title: "Hi all, I'm Sidharth Sahu",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
